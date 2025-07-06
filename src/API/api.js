76565048cda09d0964cebef061b3d0bcc1c46fad
Ricky-Prophet <studya86308@gmail.com>
2025-07-06 14:09:33 +0530

    Re-commit

--- src/API/api.js
+++ src/API/api.js
@@ -1,13 +1,3 @@
-const hostname = location.hostname;
-
-let BASE_URL;
-
-if (hostname === "localhost" || hostname === "127.0.0.1" || /^192\.168\./.test(hostname)) {
-  BASE_URL = "http://localhost:5000/api"; // Local dev
-} else {
-  BASE_URL = "https://authentication-red-kappa.vercel.app/"; // Production
-}
-
-export const AUTH_API = `${BASE_URL}`;
-export const ADMIN_AUTH_API = `${BASE_URL}/admin`;
-export const ALL_PROJECTS = `${BASE_URL}`;
+export const AUTH_API = `http://localhost:5000/api`;
+export const ADMIN_AUTH_API = `http://localhost:5000/api/admin`;
+export const ALL_PROJECTS = `http://localhost:5000/api`;
