dfaed4459286fa743a20ab7cbff37b2539c80bd0
Ricky-Prophet <studya86308@gmail.com>
2025-07-06 10:35:36 +0530

    production added

--- src/API/api.js
+++ src/API/api.js
@@ -5,7 +5,7 @@ let BASE_URL;
 if (hostname === "localhost" || hostname === "127.0.0.1" || /^192\.168\./.test(hostname)) {
   BASE_URL = "http://localhost:5000/api"; // Local dev
 } else {
-  BASE_URL = "https://your-production-api-url.com/api"; // Production
+  BASE_URL = "https://authentication-red-kappa.vercel.app/"; // Production
 }
 
 export const AUTH_API = `${BASE_URL}`;
